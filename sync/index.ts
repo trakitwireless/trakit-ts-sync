f705afaad0be6de0612bfe55d9cda75eb7d45ccd
Alex Lein <alein@trakitwireless.com>
2026-02-19 17:56:48 -0500

    Keeping npm versions in sync is a pain

--- sync/index.ts
+++ sync/index.ts
@@ -24,7 +24,7 @@ import { TrakitSocketCommander } from "./WebSocket/TrakitSocketCommander";
 /**
  * Version number for this release.
  */
-export const version = '0.0.18';
+export const version = '0.0.20';
 
 /**
  * API exports
