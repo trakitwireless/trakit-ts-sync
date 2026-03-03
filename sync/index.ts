3d99ee1b5437121e4eeecf41497d07d8e8122fba
Alex Lein <alein@trakitwireless.com>
2026-03-03 13:18:44 -0500

    Another re-publish

--- sync/index.ts
+++ sync/index.ts
@@ -24,7 +24,7 @@ import { TrakitSocketCommander } from "./WebSocket/TrakitSocketCommander";
 /**
  * Version number for this release.
  */
-export const version = '0.0.32';
+export const version = '0.0.34';
 
 /**
  * API exports
