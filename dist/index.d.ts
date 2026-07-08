21be7afe80a6d4a0706f28389fee49fbef83689b
Alex Lein <alein@trakitwireless.com>
2026-07-08 13:34:08 -0400

    Republish

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -18,7 +18,7 @@ import { TrakitSocketCommander, TrakitSocketStatus } from "./WebSocket/TrakitSoc
 /**
  * Version number for this release.
  */
-export declare const version = "0.1.5";
+export declare const version = "0.1.6";
 /**
  * API exports
  */
--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ import { TrakitSocketCommander, TrakitSocketStatus } from "./WebSocket/TrakitSoc
 /**
  * Version number for this release.
  */
-export const version = '0.1.5';
+export const version = '0.1.6';
 
 /**
  * API exports
